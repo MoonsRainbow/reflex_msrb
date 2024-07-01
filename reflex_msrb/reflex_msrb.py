f822270f274db2e17183a172a5db8ea55bd19ba4
MoonsRainbow <kalmia6863@gmail.com>
2024-07-01 22:36:09 +0900

    base initial

---
+++ reflex_msrb/reflex_msrb.py
@@ -0,0 +1,10 @@
+"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+from .pages import *
+from .states import *
+from rxconfig import config
+
+
+import reflex as rx
+
+
+app = rx.App()
